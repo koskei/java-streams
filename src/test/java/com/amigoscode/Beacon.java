0ed11cf0ffd250ba4a144268f4758810052d55e4
koskei <kyfredy@gmail.com>
2021-11-29 14:28:08 +0300

    updates on streams
    updates on streams

--- src/test/java/com/amigoscode/Beacon.java
+++ src/test/java/com/amigoscode/Beacon.java
@@ -1,15 +1,17 @@
 package com.amigoscode;
 
 import com.amigoscode.beans.Person;
+import com.amigoscode.beans.PersonDTO;
 import com.amigoscode.mockdata.MockData;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
-
 public class Beacon {
-
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(Beacon.class);
     public static void main(String[] args) throws IOException {
         testing();
     }
@@ -18,13 +20,13 @@ public class Beacon {
         HashSet<Person> persons= MockData.getPeopleHash();
 
         long no=  persons.stream().filter(p -> p.getAge()<=18).count();
-        int numbers=0;
-        for (Person  entries: persons.stream().filter(ages-> ages.getAge()<=18).collect(Collectors.toList())){
-            numbers+=1;
-            System.out.println(entries.getFirstName());
-        }
+        long numbers=persons.stream().filter(ages-> ages.getAge()<=18).limit(10).count();
+        Set<Person> ps= persons.stream().filter(ages-> ages.getAge()<=18).limit(10).collect(Collectors.toSet());
+            ps.forEach(System.out::println);
+
         System.out.println("total= "+ numbers);
         System.out.println("Total from No :"+ no);
+        log.info("testing....");
 
     }
 }
